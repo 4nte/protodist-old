8d25119249de3146c909c35b76775791bbf0e285
Ante Gulin <8070595+4nte@users.noreply.github.com>
2020-10-10 20:55:09 +0200

    Accept env params

--- cmd/root.go
+++ cmd/root.go
@@ -21,10 +21,16 @@ import (
 	"github.com/4nte/protodist/internal"
 	"github.com/go-git/go-git/v5/plumbing/transport/http"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 	"os"
+	"strings"
 
-	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/viper"
+)
+
+const (
+	envPrefix             = "PROTODIST"
+	defaultConfigFilename = "protodist"
 )
 
 var (
@@ -47,8 +53,12 @@ var rootCmd = &cobra.Command{
 	Long:  `Compile, bundle and distribute protobuf packages..`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		fmt.Println("pre run")
+		// You can bind cobra and viper in a few locations, but PersistencePreRunE on the root command works well
+		return initializeConfig(cmd)
+	},
 	Run: func(cmd *cobra.Command, args []string) {
-
 		if gitAccessToken == "" {
 			fmt.Println("warning: GIT_ACCESS_TOKEN is empty")
 		}
@@ -105,7 +115,7 @@ func Execute() {
 	protoImportPath := viper.GetStringSlice("PROTO_IMPORT_PATH")
 */
 func init() {
-	cobra.OnInitialize(initConfig)
+	//cobra.OnInitialize(initConfig)
 
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
@@ -114,6 +124,7 @@ func init() {
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.protodist.yaml)")
 	// TODO: refactor to use git_ref instead and then infer if the ref is branch or tag
 	rootCmd.PersistentFlags().StringVar(&gitBranch, "git_branch", "", "git branch")
+	rootCmd.PersistentFlags().StringVar(&gitUser, "git_user", "", "git user")
 	rootCmd.PersistentFlags().StringVar(&gitTag, "git_tag", "", "git tag")
 	rootCmd.PersistentFlags().StringVar(&gitCloneDir, "git_clone_dir", "", "git clone dir")
 	rootCmd.PersistentFlags().StringVar(&gitAccessToken, "git_access_token", "", "git access token")
@@ -135,30 +146,67 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	if err := viper.BindPFlags(rootCmd.Flags()); err != nil {
+		panic(err)
+	}
 }
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
+func bindFlags(cmd *cobra.Command, v *viper.Viper) {
+	cmd.Flags().VisitAll(func(f *pflag.Flag) {
+		// Environment variables can't have dashes in them, so bind them to their equivalent
+		// keys with underscores, e.g. --favorite-color to STING_FAVORITE_COLOR
+		fmt.Println("f", f)
+		if strings.Contains(f.Name, "-") {
+			envVarSuffix := strings.ToUpper(strings.ReplaceAll(f.Name, "-", "_"))
+			err := v.BindEnv(f.Name, fmt.Sprintf("%s_%s", envPrefix, envVarSuffix))
+			if err != nil {
+				panic(err)
+			}
 		}
 
-		// Search config in home directory with name ".protodist" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".protodist")
+		// Apply the viper config value to the flag when the flag is not set and viper has a value
+		if !f.Changed && v.IsSet(f.Name) {
+			val := v.Get(f.Name)
+			fmt.Println("setting", val)
+			err := cmd.Flags().Set(f.Name, fmt.Sprintf("%v", val))
+			if err != nil {
+				panic(err)
+			}
+		}
+	})
+}
+func initializeConfig(cmd *cobra.Command) error {
+	v := viper.New()
+
+	// Set the base name of the config file, without the file extension.
+	v.SetConfigName(defaultConfigFilename)
+
+	// Set as many paths as you like where viper should look for the
+	// config file. We are only looking in the current working directory.
+	v.AddConfigPath(".")
+
+	// Attempt to read the config file, gracefully ignoring errors
+	// caused by a config file not being found. Return an error
+	// if we cannot parse the config file.
+	if err := v.ReadInConfig(); err != nil {
+		// It's okay if there isn't a config file
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return err
+		}
 	}
 
-	viper.AutomaticEnv() // read in environment variables that match
+	// When we bind flags to environment variables expect that the
+	// environment variables are prefixed, e.g. a flag like --number
+	// binds to an environment variable STING_NUMBER. This helps
+	// avoid conflicts.
+	v.SetEnvPrefix(envPrefix)
 
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
+	// Bind to environment variables
+	// Works great for simple config names, but needs help for names
+	// like --favorite-color which we fix in the bindFlags function
+	v.AutomaticEnv()
+
+	// Bind the current command's flags to viper
+	bindFlags(cmd, v)
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/otiai10/copy v1.0.2
 	github.com/pkg/errors v0.9.1
 	github.com/spf13/cobra v1.0.0
+	github.com/spf13/pflag v1.0.3
 	github.com/spf13/viper v1.6.2
 	github.com/stretchr/testify v1.5.1 // indirect
 	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
